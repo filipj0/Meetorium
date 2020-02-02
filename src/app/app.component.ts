1b89f992c0144a9416515269762b8d8c072d34a7
INTRANET\fjakovljevic <fjakovljevic@cs.hr>
2020-02-02 09:48:22 +0100

    CSV file upload Location table populating Map initialization Initial marker adding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,99 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { MapComponent } from './components/map/map.component';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.html',
+    styleUrls: ['./app.scss']
 })
 export class AppComponent {
-  title = 'app';
+    @ViewChild('csvReader') csvReader: any;
+    @ViewChild('map') map: MapComponent;
+
+    public showUploadInput: boolean = false;
+    public showAddForm: boolean = false;
+    public locationList: Array<LocationItem> = [];
+    public imageViewerSrc: string = null;
+
+    constructor() {
+    }
+
+    public fileUploadListener($event) {
+        let files = $event.srcElement.files;
+
+        if (this.isValidCSVFile(files[0])) {
+            this.showUploadInput = false;
+            let input = $event.target;
+            let reader = new FileReader();
+            reader.readAsText(input.files[0]);
+
+            reader.onload = () => {
+                let csvData = reader.result;
+                let csvRecordsArray = csvData.split(/\r\n|\n/);
+                this.addLocationsToList(csvRecordsArray);
+            };
+
+            reader.onerror = function () {
+                console.log('error is occured while reading file!');
+            };
+
+        } else {
+            alert('Please import valid .csv file.');
+        }
+    }
+
+    private addLocationsToList(csvRecordsArray: Array<string>) {
+        // remove header array
+        let keysLength = csvRecordsArray[0].split(',').length;
+        csvRecordsArray.splice(0, 1);
+        csvRecordsArray.forEach(csvRecord => {
+            if (csvRecord.split(',').length === keysLength) {
+                this.locationList.push(new LocationItem(csvRecord.split(',')));
+            }
+        });
+        this.map.reloadMarkers(this.locationList);
+        console.log(this.locationList);
+    }
+
+    private isValidCSVFile(file: any) {
+        return file.name.endsWith('.csv');
+    }
+
+    public toggleMarkerVisibility(location: LocationItem) {
+        location.visible = !location.visible;
+        this.map.showHideMarker(location.id, location.visible);
+    }
+
+    public centerMapToLocation(location: LocationItem) {
+        this.map.centerMap(parseFloat(location.latitude), parseFloat(location.longitude));
+    }
+}
+
+export class LocationItem {
+    public id: any;
+    public companyName: any;
+    public founder: any;
+    public city: any;
+    public country: any;
+    public postalCode: any;
+    public street: any;
+    public photoUrl: any;
+    public websiteUrl: any;
+    public latitude: any;
+    public longitude: any;
+    public visible: boolean = true;
+
+    constructor(csvRecord: Array<string>) {
+        this.id = csvRecord[0].trim();
+        this.companyName = csvRecord[1].trim();
+        this.founder = csvRecord[2].trim();
+        this.city = csvRecord[3].trim();
+        this.country = csvRecord[4].trim();
+        this.postalCode = csvRecord[5].trim();
+        this.street = csvRecord[6].trim();
+        this.photoUrl = csvRecord[7].trim();
+        this.websiteUrl = csvRecord[8].trim();
+        this.latitude = csvRecord[9].trim();
+        this.longitude = csvRecord[10].trim();
+    }
 }
---
+++ src/app/components/map/map.component.ts
@@ -0,0 +1,60 @@
+import { AfterViewInit, Component, ElementRef, Input, OnChanges, OnInit, SimpleChanges, ViewChild } from '@angular/core';
+import { LocationItem } from '../../app.component';
+
+@Component({
+    selector: 'app-map',
+    templateUrl: './map.component.html',
+    styleUrls: ['./map.scss']
+})
+export class MapComponent implements AfterViewInit {
+    @ViewChild('gMap') gMap: ElementRef;
+
+    private map: google.maps.Map;
+    private markers: Array<any> = [];
+
+    constructor() {
+    }
+
+    ngAfterViewInit(): void {
+        this.initMap();
+    }
+
+    private initMap() {
+        let currentLocation = navigator.geolocation.getCurrentPosition((position: any) => {
+            let currentCoordinates = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+            let mapOptions = {
+                center: currentCoordinates,
+                zoom: 12
+            };
+
+            this.map = new google.maps.Map(this.gMap.nativeElement, mapOptions);
+        });
+    }
+
+    public reloadMarkers(locations: Array<LocationItem>) {
+        if (this.markers.length) {
+            this.markers.forEach(marker => marker.setMap(null));
+            this.markers = [];
+        }
+
+        locations.forEach((location: LocationItem) => {
+            let coordinates = new google.maps.LatLng(location.latitude, location.longitude);
+            let markerData = new google.maps.Marker({
+                position: coordinates,
+                map: this.map,
+                label: location.companyName
+            });
+            markerData.setMap(this.map);
+            this.markers.push({ id: location.id, markerData: markerData });
+        });
+    }
+
+    public showHideMarker(id: string, showMarker: boolean) {
+        let marker = this.markers.find((marker: any) => marker.id === id);
+        marker.markerData.setVisible(showMarker);
+    }
+
+    public centerMap(latitude: number, longitude: number){
+      this.map.setCenter({lat: latitude, lng: longitude});
+    }
+}
